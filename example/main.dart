7e5f8dfe42a77e6673e4ea6d9eddbf9bdf6e51e1
Andy Horn <andyjhorn@gmail.com>
2023-01-18 14:51:13 -0600

    add: example app

---
+++ example/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_resizable_container/src/resizable_child_data.dart';
+import 'package:flutter_resizable_container/src/resizable_container.dart';
+
+void main() {
+  runApp(const ExampleApp());
+}
+
+class ExampleApp extends StatelessWidget {
+  const ExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Example ResizableContainer'),
+        ),
+        body: ResizableContainer(
+          direction: Axis.horizontal,
+          children: const [
+            ResizableChildData(
+              startingRatio: 0.75,
+              minSize: 150,
+              child: Center(
+                child: Text('Left pane'),
+              ),
+            ),
+            ResizableChildData(
+              startingRatio: 0.25,
+              maxSize: 500,
+              child: Center(
+                child: Text('Right pane'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
