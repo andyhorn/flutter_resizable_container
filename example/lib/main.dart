d0b86987bf317eb722c2da54d52a7670c0b74dd2
Andy Horn <andyjhorn@gmail.com>
2023-01-19 07:53:19 -0600

    add: improve example with switchable direction

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,8 +5,15 @@ void main() {
   runApp(const ExampleApp());
 }
 
-class ExampleApp extends StatelessWidget {
+class ExampleApp extends StatefulWidget {
   const ExampleApp({super.key});
+
+  @override
+  State<ExampleApp> createState() => _ExampleAppState();
+}
+
+class _ExampleAppState extends State<ExampleApp> {
+  Axis direction = Axis.horizontal;
 
   @override
   Widget build(BuildContext context) {
@@ -15,25 +22,45 @@ class ExampleApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Example ResizableContainer'),
-        ),
-        body: ResizableContainer(
-          direction: Axis.horizontal,
-          children: const [
-            ResizableChildData(
-              startingRatio: 0.75,
-              minSize: 150,
-              child: Center(
-                child: Text('Left pane'),
-              ),
-            ),
-            ResizableChildData(
-              startingRatio: 0.25,
-              maxSize: 500,
-              child: Center(
-                child: Text('Right pane'),
-              ),
+          actions: [
+            MaterialButton(
+              onPressed: () {
+                final newDirection = direction == Axis.horizontal
+                    ? Axis.vertical
+                    : Axis.horizontal;
+
+                setState(() => direction = newDirection);
+              },
+              child: direction == Axis.horizontal
+                  ? const Text('Vertical')
+                  : const Text('Horizontal'),
             ),
           ],
+        ),
+        body: SafeArea(
+          child: ResizableContainer(
+            direction: direction,
+            children: [
+              ResizableChildData(
+                startingRatio: 0.75,
+                minSize: 150,
+                child: Center(
+                  child: direction == Axis.horizontal
+                      ? const Text('Left pane')
+                      : const Text('Top pane'),
+                ),
+              ),
+              ResizableChildData(
+                startingRatio: 0.25,
+                maxSize: 500,
+                child: Center(
+                  child: direction == Axis.horizontal
+                      ? const Text('Right pane')
+                      : const Text('Bottom pane'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
