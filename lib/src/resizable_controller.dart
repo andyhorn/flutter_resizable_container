136ea6db44fdc77c9910927c9270c6386b3dbbea
Levi Lesches <levilesches@gmail.com>
2024-02-16 03:51:16 -0500

    Documented

--- lib/src/resizable_controller.dart
+++ lib/src/resizable_controller.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
+import "resizable_child_data.dart";
+import "resizable_container.dart";
+
+/// A controller to provide a programmatic interface to a [ResizableContainer].
 class ResizableController with ChangeNotifier {
+  /// The sizes in pixels of each child.
   final List<double> sizes = [];
+  /// The total available space for this container in the given axis.
   double availableSpace = -1;
   
+  /// The number of resizable children this container has.
   int get numChildren => sizes.length;
 
+  /// The ratios of all the children, like [ResizableChildData.startingRatio].
   List<double> get ratios => [
     for (final size in sizes)
       size / availableSpace,
   ];
 
+  /// Programmatically set the ratios on the children. See [ratios] to get their current ratios.
   void setRatios(List<double> values) {
     if (values.length != numChildren) throw ArgumentError("Ratios list must be equal to the number of children");
     if (sum(values) != 1) throw ArgumentError("The sum of the ratios must equal 1");
@@ -21,6 +30,7 @@ class ResizableController with ChangeNotifier {
   }
 }
 
+/// Returns the sum of a list.
 num sum(List<num> list) {
   num result = 0;
   for (final element in list) {
