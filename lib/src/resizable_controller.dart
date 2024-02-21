ea0b956f7967340607dd88d6a5f452771cef2d4b
Andy Horn <andyjhorn@gmail.com>
2024-02-20 22:10:28 -0700

    Fix styles in ResizableController
    Fix some styles in the controller class that were causing linter warnings.

--- lib/src/resizable_controller.dart
+++ lib/src/resizable_controller.dart
@@ -8,25 +8,34 @@ import "utils.dart";
 class ResizableController with ChangeNotifier {
   /// The sizes in pixels of each child.
   final List<double> sizes = [];
+
   /// The total available space for this container in the given axis.
   double availableSpace = -1;
-  
+
   /// The number of resizable children this container has.
   int get numChildren => sizes.length;
 
   /// The ratios of all the children, like [ResizableChildData.startingRatio].
   List<double> get ratios => [
-    for (final size in sizes)
-      size / availableSpace,
-  ];
+        for (final size in sizes) size / availableSpace,
+      ];
 
   /// Programmatically set the ratios on the children. See [ratios] to get their current ratios.
   void setRatios(List<double> values) {
-    if (values.length != numChildren) throw ArgumentError("Ratios list must be equal to the number of children");
-    if (sum(values) != 1) throw ArgumentError("The sum of the ratios must equal 1");
+    if (values.length != numChildren) {
+      throw ArgumentError(
+        "Ratios list must be equal to the number of children",
+      );
+    }
+
+    if (sum(values) != 1) {
+      throw ArgumentError("The sum of the ratios must equal 1");
+    }
+
     for (var i = 0; i < numChildren; i++) {
       sizes[i] = values[i] * availableSpace;
     }
+
     notifyListeners();
   }
 }
