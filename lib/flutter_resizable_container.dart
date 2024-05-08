78aedae2bde94fe3588fa2ce6385cd8fab9debca
Andy Horn <andyjhorn@gmail.com>
2024-05-08 13:02:01 -0700

    Add ResizableDivider config class

--- lib/flutter_resizable_container.dart
+++ lib/flutter_resizable_container.dart
@@ -3,3 +3,4 @@ library flutter_resizable_container;
 export 'src/resizable_container.dart';
 export 'src/resizable_controller.dart';
 export 'src/resizable_child_data.dart';
+export 'src/resizable_divider.dart';
