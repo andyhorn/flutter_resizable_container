25191fd0109c415a4f56ccd97f5945fddd204be8
Andy Horn <andyjhorn@gmail.com>
2024-05-09 12:17:37 -0700

    Fix ResizableChild tests

--- test/resizable_child_test.dart
+++ test/resizable_child_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_resizable_container/src/resizable_child.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,42 +7,42 @@ void main() {
     group('startingRatio', () {
       test('throws if less than 0', () {
         expect(
-          () => ResizableChild(startingRatio: -0.1),
+          () => ResizableChild(startingRatio: -0.1, child: const SizedBox()),
           throwsAssertionError,
         );
       });
 
       test('throws if greater than 1', () {
         expect(
-          () => ResizableChild(startingRatio: 1.1),
+          () => ResizableChild(startingRatio: 1.1, child: const SizedBox()),
           throwsAssertionError,
         );
       });
 
       test('allows null', () {
         expect(
-          () => const ResizableChild(startingRatio: null),
+          () => const ResizableChild(startingRatio: null, child: SizedBox()),
           isNot(throwsA(anything)),
         );
       });
 
       test('allows a value between 0 and 1', () {
         expect(
-          () => const ResizableChild(startingRatio: 0.5),
+          () => const ResizableChild(startingRatio: 0.5, child: SizedBox()),
           isNot(throwsA(anything)),
         );
       });
 
       test('allows 0', () {
         expect(
-          () => const ResizableChild(startingRatio: 0),
+          () => const ResizableChild(startingRatio: 0, child: SizedBox()),
           isNot(throwsA(anything)),
         );
       });
 
       test('allows 1', () {
         expect(
-          () => const ResizableChild(startingRatio: 1),
+          () => const ResizableChild(startingRatio: 1, child: SizedBox()),
           isNot(throwsA(anything)),
         );
       });
