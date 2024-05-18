faad62899624f44d1e909a33851079b3343e8811
Andy Horn <andyjhorn@gmail.com>
2024-05-18 11:49:54 -0700

    Add overrides to ResizableStartingSize children

--- lib/src/resizable_starting_size.dart
+++ lib/src/resizable_starting_size.dart
@@ -11,8 +11,28 @@ sealed class ResizableStartingSize {
 
 final class ResizableStartingSizePixels extends ResizableStartingSize {
   const ResizableStartingSizePixels._(double pixels) : super._(pixels);
+
+  @override
+  String toString() => 'ResizableStartingSize(pixels: $value)';
+
+  @override
+  operator ==(Object other) =>
+      other is ResizableStartingSizePixels && other.value == value;
+
+  @override
+  int get hashCode => Object.hash(runtimeType, value);
 }
 
 final class ResizableStartingSizeRatio extends ResizableStartingSize {
   const ResizableStartingSizeRatio._(double ratio) : super._(ratio);
+
+  @override
+  String toString() => 'ResizableStartingSize(ratio: $value)';
+
+  @override
+  operator ==(Object other) =>
+      other is ResizableStartingSizeRatio && other.value == value;
+
+  @override
+  int get hashCode => Object.hash(runtimeType, value);
 }
