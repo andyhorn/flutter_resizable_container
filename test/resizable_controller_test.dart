934a8d4ef7dc35016b3381f780ceba8ad6253cd3
Andy Horn <andyjhorn@gmail.com>
2024-05-19 23:02:23 -0700

    Rewrite ResizableController tests

--- test/resizable_controller_test.dart
+++ test/resizable_controller_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_resizable_container/flutter_resizable_container.dart';
+import 'package:flutter_resizable_container/src/resizable_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -12,82 +13,354 @@ void main() {
 
     tearDown(() => controller.dispose());
 
-    group('ratios', () {
+    group('.sizes', () {
       setUp(() {
-        controller.setChildren(
-          const [
+        controller.setChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          ResizableChild(
+            startingSize: ResizableSize.pixels(200),
+            child: const SizedBox.shrink(),
+          ),
+        ]);
+
+        controller.setAvailableSpace(300);
+      });
+
+      test('returns a list of sizes', () {
+        expect(controller.sizes, equals([100, 200]));
+      });
+    });
+
+    group('.ratios', () {
+      setUp(() {
+        controller.setChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          ResizableChild(
+            startingSize: ResizableSize.pixels(200),
+            child: const SizedBox.shrink(),
+          ),
+        ]);
+
+        controller.setAvailableSpace(300);
+      });
+
+      test('returns a list of ratios', () {
+        expect(controller.ratios, equals([1 / 3, 2 / 3]));
+      });
+    });
+
+    group('#setAvailableSpace', () {
+      group('when the new value is the same', () {
+        setUp(() {
+          controller.setChildren([
             ResizableChild(
-              startingRatio: 0.1,
-              child: SizedBox(),
+              startingSize: ResizableSize.pixels(100),
+              child: const SizedBox.shrink(),
             ),
             ResizableChild(
-              startingRatio: 0.1,
-              child: SizedBox(),
+              startingSize: ResizableSize.pixels(200),
+              child: const SizedBox.shrink(),
             ),
-            ResizableChild(
-              startingRatio: 0.25,
-              child: SizedBox(),
-            ),
-            ResizableChild(
-              startingRatio: 0.25,
-              child: SizedBox(),
-            ),
-            ResizableChild(
-              startingRatio: 0.3,
-              child: SizedBox(),
-            ),
-          ],
-        );
+          ]);
 
-        controller.availableSpace = 1000;
-      });
-
-      test('returns the ratios of all the children', () {
-        expect(controller.ratios, [0.1, 0.1, 0.25, 0.25, 0.3]);
-      });
-
-      test('changes the ratios', () {
-        controller.ratios = [0.2, 0.2, 0.2, 0.2, 0.2];
-        expect(controller.sizes, equals([200, 200, 200, 200, 200]));
-      });
-
-      test('throws an error if the list is the wrong length', () {
-        expect(
-          () => controller.ratios = [0.2, 0.2, 0.2],
-          throwsArgumentError,
-        );
-      });
-
-      test('throws an error if any ratio is less than 0', () {
-        expect(
-          () => controller.ratios = [0.2, 0.2, -0.2, 0.2, 0.2],
-          throwsArgumentError,
-        );
-      });
-
-      test('throws an error if any ratio is greater than 1', () {
-        expect(
-          () => controller.ratios = [0.2, 0.2, 1.2, 0.2, 0.2],
-          throwsArgumentError,
-        );
-      });
-
-      test('throws an error if the sum of all ratios is greater than 1.0', () {
-        expect(
-          () => controller.ratios = [0.2, 0.5, 0.2, 0.2, 0.2],
-          throwsArgumentError,
-        );
-      });
-
-      test('updates listeners', () {
-        bool didUpdate = false;
-        controller.addListener(() {
-          didUpdate = true;
+          controller.setAvailableSpace(100);
         });
 
-        controller.ratios = [0.2, 0.2, 0.2, 0.2, 0.2];
+        test('does not notify listeners', () {
+          var notified = false;
+          controller.addListener(() => notified = true);
 
-        expect(didUpdate, isTrue);
+          controller.setAvailableSpace(100);
+
+          expect(notified, isFalse);
+        });
+      });
+
+      group('when setting the value for the first time', () {
+        setUp(() {
+          controller.setChildren([
+            ResizableChild(
+              startingSize: ResizableSize.pixels(100),
+              child: const SizedBox.shrink(),
+            ),
+            ResizableChild(
+              startingSize: ResizableSize.ratio(1 / 2),
+              child: const SizedBox.shrink(),
+            ),
+            const ResizableChild(child: SizedBox.shrink()),
+          ]);
+        });
+
+        test('sets sizes based on child starting size', () {
+          controller.setAvailableSpace(300);
+          expect(controller.sizes, equals([100, 100, 100]));
+        });
+
+        test('notifies listeners', () {
+          var notified = false;
+          controller.addListener(() => notified = true);
+          controller.setAvailableSpace(300);
+          expect(notified, isTrue);
+        });
+      });
+
+      group('when updating the available space', () {
+        setUp(() {
+          controller.setChildren([
+            ResizableChild(
+              startingSize: ResizableSize.pixels(100),
+              child: const SizedBox.shrink(),
+            ),
+            ResizableChild(
+              startingSize: ResizableSize.ratio(1 / 2),
+              child: const SizedBox.shrink(),
+            ),
+            const ResizableChild(child: SizedBox.shrink()),
+          ]);
+
+          controller.setAvailableSpace(300);
+        });
+
+        test('adjusts child sizes', () {
+          controller.setAvailableSpace(600);
+          expect(controller.sizes, equals([200, 200, 200]));
+        });
+
+        test('notifies listeners', () {
+          var notified = false;
+          controller.addListener(() => notified = true);
+          controller.setAvailableSpace(600);
+          expect(notified, isTrue);
+        });
+      });
+    });
+
+    group('#setChildren', () {
+      test('sets the list of ResizableChild', () {
+        controller.setChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+        ]);
+
+        expect(
+          ResizableControllerTestHelper.getChildren(controller),
+          equals(
+            [
+              ResizableChild(
+                startingSize: ResizableSize.pixels(100),
+                child: const SizedBox.shrink(),
+              ),
+            ],
+          ),
+        );
+      });
+
+      test('does not notify listeners', () {
+        var notified = false;
+        controller.addListener(() => notified = true);
+        controller.setChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+        ]);
+        expect(notified, isFalse);
+      });
+    });
+
+    group('#updateChildren', () {
+      setUp(() {
+        controller.setChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+        ]);
+
+        controller.setAvailableSpace(200);
+      });
+
+      test('sets the list of children', () {
+        controller.updateChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+        ]);
+
+        expect(
+          ResizableControllerTestHelper.getChildren(controller).length,
+          equals(3),
+        );
+      });
+
+      test('updates children sizes', () {
+        controller.updateChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+        ]);
+
+        expect(controller.sizes, equals([100, 50, 50]));
+      });
+
+      test('notifies listeners', () {
+        var notified = false;
+        controller.addListener(() => notified = true);
+        controller.updateChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+        ]);
+
+        expect(notified, isTrue);
+      });
+    });
+
+    group('#adjustChildSize', () {
+      setUp(() {
+        controller.setChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+        ]);
+
+        controller.setAvailableSpace(200);
+      });
+
+      group('when increasing the size', () {
+        test('increases the size of the target child', () {
+          controller.adjustChildSize(index: 1, delta: 10);
+          expect(controller.sizes[1], equals(60));
+        });
+
+        test('decreases the size of the adjacent child', () {
+          controller.adjustChildSize(index: 1, delta: 10);
+          expect(controller.sizes[2], equals(40));
+        });
+
+        test('notifies listeners', () {
+          var notified = false;
+          controller.addListener(() => notified = true);
+          controller.adjustChildSize(index: 1, delta: 10);
+          expect(notified, isTrue);
+        });
+      });
+    });
+
+    group('#setSizes', () {
+      setUp(() {
+        controller.setChildren([
+          ResizableChild(
+            startingSize: ResizableSize.pixels(100),
+            child: const SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+          const ResizableChild(
+            startingSize: null,
+            child: SizedBox.shrink(),
+          ),
+        ]);
+
+        controller.setAvailableSpace(200);
+      });
+
+      group('when the list is the wrong length', () {
+        test('throws argument error', () {
+          expect(
+            () => controller.setSizes([
+              ResizableSize.pixels(100),
+              ResizableSize.pixels(100),
+            ]),
+            throwsArgumentError,
+          );
+        });
+      });
+
+      group('when the number of pixels exceeds the available space', () {
+        test('throws an argument error', () {
+          expect(
+            () => controller.setSizes([
+              ResizableSize.pixels(150),
+              ResizableSize.pixels(150),
+              ResizableSize.pixels(150),
+            ]),
+            throwsArgumentError,
+          );
+        });
+      });
+
+      group('when the ratio sum exceeds 1', () {
+        test('throws an argument error', () {
+          expect(
+            () => controller.setSizes([
+              ResizableSize.ratio(0.5),
+              ResizableSize.ratio(0.5),
+              ResizableSize.ratio(0.5),
+            ]),
+            throwsArgumentError,
+          );
+        });
+      });
+
+      test('sets child sizes', () {
+        controller.setSizes([
+          ResizableSize.pixels(100),
+          ResizableSize.ratio(0.5),
+          null,
+        ]);
+
+        expect(controller.sizes, equals([100, 50, 50]));
       });
     });
   });
