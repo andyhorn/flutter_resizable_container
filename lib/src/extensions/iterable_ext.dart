56c96ef1de75e4d41cfbb594b98da2a36fb5cfbd
Andy Horn <andyjhorn@gmail.com>
2024-05-21 12:18:43 -0700

    Add more iterable extensions

--- lib/src/extensions/iterable_ext.dart
+++ lib/src/extensions/iterable_ext.dart
@@ -1,7 +1,21 @@
+import 'package:flutter_resizable_container/flutter_resizable_container.dart';
+
 extension IterableNumExtensions on Iterable<num> {
   num sum() => fold(0, (sum, current) => sum + current);
 }
 
-extension IterableExtensions on Iterable {
+extension IterableExtensions<T> on Iterable<T> {
   int nullCount() => where((item) => item == null).length;
+  int count(bool Function(T) test) => where(test).length;
+  num sum(num Function(T) extractor) =>
+      fold(0.0, (sum, current) => sum + extractor(current));
+}
+
+extension ResizableSizeIterableExtensions on Iterable<ResizableSize> {
+  double get totalPixels =>
+      where((size) => size.isPixels).sum((size) => size.value).toDouble();
+  double get totalRatio =>
+      where((size) => size.isRatio).sum((size) => size.value).toDouble();
+  int get flexCount =>
+      where((size) => size.isExpand).sum((size) => size.value).toInt();
 }
