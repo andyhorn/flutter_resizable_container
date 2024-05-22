ff284f51ba75ce5be54ff961d3714f89d6cd92ce
Andy Horn <andyjhorn@gmail.com>
2024-05-22 08:16:04 -0700

    Add NavDrawer

---
+++ example/lib/widgets/nav_drawer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  const NavDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text('Basic Example'),
+              onTap: () => Navigator.of(context).pushReplacementNamed('basic'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
