8ec0ba203cd482e2141dc96c70addf6d456655d1
Andy Horn <andyjhorn@gmail.com>
2024-05-22 08:51:39 -0700

    Improve NavDrawer

--- example/lib/widgets/nav_drawer.dart
+++ example/lib/widgets/nav_drawer.dart
@@ -6,23 +6,27 @@ class NavDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 8,
-        ),
-        child: Column(
-          children: [
-            ListTile(
-              title: const Text('Basic Example'),
-              onTap: () => Navigator.of(context).pushReplacementNamed('basic'),
-            ),
-            ListTile(
-              title: const Text('Ratio Example'),
-              onTap: () => Navigator.of(context).pushReplacementNamed('ratio'),
-            ),
-          ],
-        ),
+      child: Column(
+        children: [
+          const SizedBox(height: 15),
+          Text(
+            'Examples',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(height: 15),
+          ListTile(
+            title: const Text('Basic Example'),
+            onTap: () => Navigator.of(context).pushReplacementNamed('basic'),
+          ),
+          ListTile(
+            title: const Text('Ratio Example'),
+            onTap: () => Navigator.of(context).pushReplacementNamed('ratio'),
+          ),
+          ListTile(
+            title: const Text('Pixels Example'),
+            onTap: () => Navigator.of(context).pushReplacementNamed('pixels'),
+          ),
+        ],
       ),
     );
   }
