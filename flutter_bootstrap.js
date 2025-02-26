96d8f4713958b39ebe43519b96d5b187294c8133
andyhorn <andyjhorn@gmail.com>
2025-02-26 15:27:19 +0000

    Deploying to gh-pages from @ andyhorn/flutter_resizable_container@7d5e7dfba8f28d3abca9494e3140b990e1846a13 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3472927247"
+    serviceWorkerVersion: "2602636907"
   }
 });
